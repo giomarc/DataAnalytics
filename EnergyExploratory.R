57b62d756f95c093f3d6bee4c8093b0ba7b9bed3
Peter Shah <peter.shah@levlnow.com>
2017-06-23 12:06:56 -0700

    Meh

--- EnergyExploratory.R
+++ EnergyExploratory.R
@@ -7,6 +7,13 @@ str(energydata)
 summary(energydata)
 
 # histogram all values are they really numerics? 
+require(ggplot2)
+histFun <- function(dframe, column)
+{
+  ggplot(dframe, aes(column)) + geom_histogram()
+}
+
+lapply(energydata[, -facCol], histFun(energydata, x))
 
 # histogram of glazing area is this really a numeric?
 
