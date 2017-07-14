6203217f2206fba12c2775fa70072add894a7405
Peter Shah <peter.shah@levlnow.com>
2017-07-14 09:53:15 -0700

    Build stat table in sim

--- SimulationAssign3.R
+++ SimulationAssign3.R
@@ -1,6 +1,8 @@
+
+
 doors = c(1,2,3)
-doorCar <- sample(doors, size = 1000, replace = TRUE, prob = c(1/3, 1/3, 1/3))
-doorChoosen <- sample(doors, size = 1000, replace = TRUE, prob = c(1/3, 1/3, 1/3))
+doorCar <- sample(doors, size = n, replace = TRUE, prob = c(1/3, 1/3, 1/3))
+doorChoosen <- sample(doors, size = n, replace = TRUE, prob = c(1/3, 1/3, 1/3))
 simMonty <- data.frame(doorCar = doorCar, doorChoosen = doorChoosen)
 
 
@@ -19,12 +21,25 @@ noSwitchWin <- apply(simMonty, 1, function(x) ifelse(x[1]==x[5], 1,0))
 simMonty <- cbind(simMonty, switchWin = switchWin, noSwitchWin = noSwitchWin)
 
 sum(simMonty$switchWin)
+
 sum(simMonty$noSwitchWin)
 
-hist(simMonty$switchWin)
-mean(simMonty$switchWin)
-var(simMonty$switchWin)
 
+hist(simMonty$switchWin)
 hist(simMonty$noSwitchWin)
-mean(simMonty$noSwitchWin)
-var(simMonty$noSwitchWin)
+
+a <- c(mean(simMonty$switchWin), 
+       var(simMonty$switchWin), 
+       length(simMonty$switchWin[simMonty$switchWin == 0]), 
+       length(simMonty$switchWin[simMonty$switchWin == 1]), n)
+
+b <- c(mean(simMonty$noSwitchWin), 
+       var(simMonty$noSwitchWin), 
+       length(simMonty$noSwitchWin[simMonty$noSwitchWin == 0]), 
+       length(simMonty$noSwitchWin[simMonty$noSwitchWin == 1]), n)
+
+c <- as.data.frame(rbind(a, b))
+row.names(c) <- c("Switching", "No Switching")
+colnames(c) <- c("mean_winProb", "variance", "Wins", "Losses", "Iterations")
+
+return(c)
