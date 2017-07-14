6e471ab1f8443987de8f1bac6b34c0fa27bd5525
petershahlevlnow <pshah82@gmail.com>
2017-07-13 22:42:01 -0700

    summary stats and hist

--- SimulationAssign3.R
+++ SimulationAssign3.R
@@ -1,6 +1,6 @@
 doors = c(1,2,3)
-doorCar <- sample(doors, size = 10, replace = TRUE, prob = c(1/3, 1/3, 1/3))
-doorChoosen <- sample(doors, size = 10, replace = TRUE, prob = c(1/3, 1/3, 1/3))
+doorCar <- sample(doors, size = 1000, replace = TRUE, prob = c(1/3, 1/3, 1/3))
+doorChoosen <- sample(doors, size = 1000, replace = TRUE, prob = c(1/3, 1/3, 1/3))
 simMonty <- data.frame(doorCar = doorCar, doorChoosen = doorChoosen)
 
 
@@ -17,3 +17,14 @@ switchWin <- apply(simMonty, 1, function(x) ifelse(x[1]==x[4], 1, 0))
 noSwitchWin <- apply(simMonty, 1, function(x) ifelse(x[1]==x[5], 1,0))
 
 simMonty <- cbind(simMonty, switchWin = switchWin, noSwitchWin = noSwitchWin)
+
+sum(simMonty$switchWin)
+sum(simMonty$noSwitchWin)
+
+hist(simMonty$switchWin)
+mean(simMonty$switchWin)
+var(simMonty$switchWin)
+
+hist(simMonty$noSwitchWin)
+mean(simMonty$noSwitchWin)
+var(simMonty$noSwitchWin)
