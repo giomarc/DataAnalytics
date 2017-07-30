1f1f4b7f959f824476bafadff421028b4af76430
petershahlevlnow <pshah82@gmail.com>
2017-07-30 02:33:29 -0700

    layout

--- BootCompAss5.R
+++ BootCompAss5.R
@@ -11,6 +11,14 @@ boot.comb <- function(a = auto.price, f = auto.price$body.style, n = 100){
     split.a.boot <- cbind(split.a.boot, one.boot(split.a[[j]]$log.price, mean, R = n)$t)
   }
   
+  # set layout
+  # create empty matrix
+  # ma <- matrix(0, 4, 4)
+  # loop through combn row 1, 
+  
+  layout(matrix(c(1,2,3,4,5,6,7,0,8,9,0,0,10,0,0,0), nrow = 4, byrow=T))
+  #layout(matrix(1:ncol(y), nrow = 2, byrow=T))
+  
   for(x in 1:ncol(y)){
     x.1 <- y[1,x]
     x.2 <- y[2,x]
@@ -25,13 +33,18 @@ boot.comb <- function(a = auto.price, f = auto.price$body.style, n = 100){
             as.numeric(t$p.value))
     r <- as.data.frame(t(r))
     df <- rbind(df, r)
-    #two.boot.mean <- two.boot(split.a auto.diesel$log.price, mean, R = 100000)
+    two.boot.mean <- two.boot(split.a[[x.1]]$log.price, split.a[[x.2]]$log.price, mean, R = n)
+    plot.diff(two.boot.mean$t, cols = paste(names.col[x.1], names.col[x.2], sep = "-"))
   }
+  
   colnames(df) <- c('body.1', 'body.2', 
                     'mean.1', 'mean.2 ',
                     'mean.diff', 'lower', 'upper', 'p.value')
   df[, 3:ncol(df)] <- lapply(df[,3:ncol(df)], as.character)
   df[, 3:ncol(df)] <- lapply(df[,3:ncol(df)], as.numeric)
   df[, 3:ncol(df)] <- lapply(df[,3:ncol(df)], round, digits = 5)
+  
+  par(mfrow =c(1,1))
+  
   return(df)
 }
