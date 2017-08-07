c266ea8d73345c3cd0314ed88445bf75735197eb
Peter Shah <peter.shah@levlnow.com>
2017-08-07 10:50:25 -0700

    Added names to plots

--- BayesCarsAss6.R
+++ BayesCarsAss6.R
@@ -23,34 +23,35 @@ posterior = function(prior, like){
   post / sum(post) # Normalize and return
 }
 
-plot.post = function(prior, like, post, x){
+plot.post = function(prior, like, post, x, name = 'test'){
   maxy = max(c(prior, like, post))
   plot(x, like, , lty = 1, ylim = c(0.0, maxy), 
        ylab = 'Density', xlab = 'Parameter value',
-       main = 'Density of prior, likelihood, posterior',
+       main = paste(name, '- Density of prior, likelihood, posterior'),
        lwd = 2, col = 'green')
   lines(x, prior, lty = 2, lwd = 2, col = 'blue')    
   lines(x, post, lty = 1, lwd = 2, col = 'red')
   legend('topright', c('likelihood', 'prior', 'posterior'), 
          lty=1, col=c('green', 'blue', 'red'), bty='n', cex=1.0)
   
-  cat(' Maximum of prior density =', round(x[which.max(prior)], 3), '\n',
+  cat(name, ':\n',
+      'Maximum of prior density =', round(x[which.max(prior)], 3), '\n',
       'Maximum likelihood =', round(x[which.max(like)], 3), '\n',
       'MAP =', round(x[which.max(post)], 3))
 }
 
-comp.like = function(p, x){
+comp.like = function(p, x, name = 'a'){
   l = rep(0, length = length(p))
   sigmaSqr = sd(x)^2
   xBar = mean(x)
-  cat(' Mean =', xBar, 'Standard deviation =', sqrt(sigmaSqr), '\n')
+  cat(name, ' Mean =', xBar, 'Standard deviation =', sqrt(sigmaSqr), '\n')
   n = length(x)
   #    l = sapply(p, function(u) dnorm(u, mean = xBar, sd = sigmaSqr))
   l = sapply(p, function(u) exp(- n* (xBar - u)^2 / (2 * sigmaSqr)))
   l / sum(l) # Normalize and return
 }
 
-plot.ci = function(p, post, nSamps, qs){
+plot.ci = function(p, post, nSamps, qs, name = "test"){
   ## This function computes a credible interval using an assumption
   ## of symetry in the bulk of the distribution to keep the 
   ## calculation simple. 
@@ -60,36 +61,36 @@ plot.ci = function(p, post, nSamps, qs){
   
   ## Plot the density with the credible interval
   interval = qs[2] - qs[1]
-  title = paste('Posterior density with', interval, 'credible interval')
+  title = paste(name, ' - Posterior density with', interval, 'credible interval')
   plot(p, post, , typ = 'l', ylab = 'Density', xlab = 'Parameter value',
        main = title, lwd = 2, col = 'blue')
   abline(v = ci[1], col = 'red', lty = 2, lwd = 2)
   abline(v = ci[2], col = 'red', lty = 2, lwd = 2)
   cat('The', interval, 'Credible interval is', 
-      round(ci[1], 2), 'to', round(ci[2], 2))
+      round(ci[1], 2), 'to', round(ci[2], 2), 'for', name)
 }
 
-bayes.pairs <- function(a, b, p, pp,   nSamps = 100000, qs = c(0.025, 0.975)){
+bayes.pairs <- function(a, b, p, pp,   nSamps = 100000, qs = c(0.025, 0.975), name = c('a', 'b')){
   
   # sort log prices
   a <- sort(a, decreasing = FALSE)
   b <- sort(b, decreasing = FALSE)
   
   # calculate liklihood and posterior
-  like.a = comp.like(p, a)
+  like.a = comp.like(p, a, name[1])
   post.a = posterior(pp, like.a)
   
-  like.b = comp.like(p, b)
+  like.b = comp.like(p, b, name[2])
   post.b = posterior(pp, like.b)
   
   # plot ci 
   nSamps = 100000
   qs = c(0.025, 0.975)
-  plot.ci(p, post.a, nSamps, qs)
-  plot.ci(p, post.b, nSamps, qs)
+  plot.ci(p, post.a, nSamps, qs, name[1])
+  plot.ci(p, post.b, nSamps, qs, name[2])
   
-  plot.post(pp, like.a, post.a, p)
-  plot.post(pp, like.b, post.b, p)
+  plot.post(pp, like.a, post.a, p, name[1])
+  plot.post(pp, like.b, post.b, p, name[2])
   
 }
 
@@ -105,7 +106,7 @@ require(dplyr)
 auto.turbo <- filter(auto.price, aspiration == "turbo")
 auto.standard <- filter(auto.price, aspiration == "std")
 
-bayes.pairs(auto.turbo$log.price, auto.standard$log.price, p = p, pp = pp)
+bayes.pairs(auto.turbo$log.price, auto.standard$log.price, p = p, pp = pp, name = c('Turbo', 'Standard'))
 
 # sort log prices
 auto.turbo$log.price <- sort(auto.turbo$log.price, decreasing = FALSE)
@@ -131,7 +132,7 @@ plot.post(pp, like.auto.standard, post.auto.standard, p)
 auto.gas <- filter(auto.price, fuel.type == "gas")
 auto.diesel <- filter(auto.price, fuel.type == "diesel")
 
-bayes.pairs(auto.gas$log.price, auto.diesel$log.price, p =p, pp = pp)
+bayes.pairs(auto.gas$log.price, auto.diesel$log.price, p =p, pp = pp, name = c('Gas', "Diesel"))
 
 
 #### Body Style 
