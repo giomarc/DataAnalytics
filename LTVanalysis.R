d9d7254a869bff9260fc385c4ea754915e0084a8
Peter Shah <peter.shah@levlnow.com>
2017-08-21 12:31:50 -0700

    Add player sample

--- LTVanalysis.R
+++ LTVanalysis.R
@@ -1,7 +1,11 @@
-# load 
+# load data
 
-file <- "ltvsampl.txt"
+file <- "playersample.txt"
 players <- read.table(file, header = TRUE, stringsAsFactors = FALSE)
 str(players)
+
+head(players)
+
+summary(players)
 
 max(players$BOOKINGS_3)
