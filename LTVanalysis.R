021f80643ebc1f8583caf1352da91ec7d777cab2
Peter Shah <peter.shah@levlnow.com>
2017-08-22 17:01:53 -0700

    Exploratory Analysis

--- LTVanalysis.R
+++ LTVanalysis.R
@@ -26,12 +26,89 @@ players <- data.frame(lapply(players, function(x) if(is.character(x)) as.factor(
 
 #create a purchaser column, fails, success.rate column
 players <- players %>% mutate(purchaser_30 = PURCHASES_30 > 0)
+players <- players %>% mutate(purchaser_3 = PURCHASES_3 > 0)
 players <- players %>% mutate(fails = (ATTEMPTS + RETRYS) - COMPLETES)
-players <- players %>% mutate(success.rate = ifelse(ATTEMPTS > 0, COMPLETES/(ATTEMPTS + RETRYS), 0))
+players <- players %>% mutate(success.rate = ifelse(ATTEMPTS > 0, COMPLETES/(ATTEMPTS), 0))
 
 #understand payer summary vs non-payers
 
 summary(filter(players, purchaser_30 == TRUE))
 summary(filter(players, purchaser_30 == FALSE))
+summary(filter(players, SESSIONS == 1))
 
-nrow(filter(players, ATTEMPTS > 1))
+
+players %>% filter(SESSIONS == 1)  %>% count(PURCHASES_30)
+
+# bar plots of purchasers/purchases
+
+require(ggplot2)
+require(gridExtra)
+
+# How many purchasers are there as a proportion of players monetize after 3 days and after 30 days 
+
+p1 <- ggplot(players, aes(x = as.factor(purchaser_3))) +
+  geom_bar(aes(y = (..count..)/sum(..count..))) +
+  geom_text(aes(y = ((..count..)/sum(..count..)), label = scales::percent((..count..)/sum(..count..))), stat = "count", vjust = -0.25) +
+  scale_y_continuous(labels = scales::percent) +
+  labs(title = "Monetizers after 3 days", y = "Percent", x = "Monetizer")
+
+p2 <- ggplot(players, aes(x = as.factor(purchaser_30))) +
+  geom_bar(aes(y = (..count..)/sum(..count..))) +
+  geom_text(aes(y = ((..count..)/sum(..count..)), label = scales::percent((..count..)/sum(..count..))), stat = "count", vjust = -0.25) +
+  scale_y_continuous(labels = scales::percent) +
+  labs(title = "Monetizers after 30 days", y = "Percent", x = "Monetizer")
+
+grid.arrange(p1, p2, ncol = 2)
+
+# Examine 
+
+ggplot(players) + stat_count(mapping = aes(x=purchaser_30, y=..prop.., group=1))
+
+# Monetizers sessiontime
+bw1 = (max(players$SESSIONS) - min(players$SESSIONS))/200
+ggplot(players, aes(SESSIONS)) + 
+  geom_histogram(binwidth = bw1) + 
+  xlab('Sessions (sec.)') +
+  ylab('Counts') +
+  xlim(NA, 100) +
+  ggtitle('Session Time first 3 days')
+
+# plot session time for 30 day purchasers
+
+a <- players %>% filter(purchaser_30 == TRUE) %>% select(SESHTIME)  
+b <- players %>% filter(purchaser_30 == FALSE) %>% select(SESHTIME) 
+plot.t(a$SESHTIME, b$SESHTIME, cols = c('First 3 days Session Time (sec.) Monetizer', 'First 3 days Session Time (sec.) Non-Monetizer'))
+
+# plot session time for 30 day purchasers
+
+a <- players %>% filter(purchaser_30 == TRUE & SESHTIME > 0) %>% select(SESHTIME)  
+b <- players %>% filter(purchaser_30 == FALSE & SESHTIME > 0) %>% select(SESHTIME) 
+plot.t(a$SESHTIME, b$SESHTIME, cols = c('First 3 days Session Time (sec.) Monetizer', 'First 3 days Session Time (sec.) Non-Monetizer'))
+
+# plot sinks time for 30 day purchasers
+
+a <- players %>% filter(purchaser_30 == TRUE & SESHTIME > 0) %>% select(TOTALSINKS)  
+b <- players %>% filter(purchaser_30 == FALSE & SESHTIME > 0) %>% select(TOTALSINKS) 
+plot.t(a$TOTALSINKS, b$TOTALSINKS, cols = c('First 3 days Sinks Monetizer', 'First 3 days Sinks Non-Monetizer'))
+
+plot.dists.cut(a$TOTALSINKS, b$TOTALSINKS, 
+               cols = c('First 3 days Sinks Monetizer', 'First 3 days Sinks Non-Monetizer'), 
+               nbins = 160)
+
+# plot last course time for 30 day purchasers
+
+a <- players %>% filter(purchaser_30 == TRUE & SESHTIME > 0) %>% select(LASTCOURSE)  
+b <- players %>% filter(purchaser_30 == FALSE & SESHTIME > 0) %>% select(LASTCOURSE) 
+plot.dists(a$LASTCOURSE, b$LASTCOURSE, cols = c('Last Course Reached Day 3 Monetizer', 'Last Course Reached Day 3 Non-Monetizer'), nbins = 500)
+
+
+plot.dists.cut(a$LASTCOURSE, b$LASTCOURSE, cols = c('Last Course Reached Day 3 Monetizer', 'Last Course Reached Day 3 Non-Monetizer'), nbins = 838)
+
+
+players.sub <- players %>% filter((SESSIONS > 1 & SESHTIME >0) | purchaser_30 == TRUE)
+summary(players.sub)
+
+ggplot(sample_frac(players.sub, 0.10), aes(SESHTIME, TOTALSINKS)) + 
+  geom_point(aes(color = factor(purchaser_30), size = BOOKINGS_30), alpha = 0.3) + 
+  xlab('Session Time (sec.)') + ylab('Total Sinks') + 
+  ggtitle('Engagement vs. Sinking')
---
+++ PlotFunctions.R
@@ -0,0 +1,43 @@
+plot.hist <- function(a, maxs, mins, cols = 'difference of means', nbins = 80, p = 0.05) {
+  breaks = seq(maxs, mins, length.out = (nbins + 1))
+  hist(a, breaks = breaks, main = paste('Histogram of', cols), xlab = cols)
+  abline(v = mean(a), lwd = 4, col = 'red')
+  abline(v = 0, lwd = 4, col = 'blue')
+  abline(v = quantile(a, probs = p/2), lty = 3, col = 'red', lwd = 3)  
+  abline(v = quantile(a, probs = (1 - p/2)), lty = 3, col = 'red', lwd = 3)
+}
+
+plot.t <- function(a, b, cols = c('pop_A', 'pop_B'), nbins = 80, p = 0.05){
+  maxs = max(c(max(a), max(b)))
+  mins = min(c(min(a), min(b)))
+  par(mfrow = c(2, 1))
+  plot.hist(a, maxs, mins, cols = cols[1])
+  plot.hist(b, maxs, mins, cols = cols[2])
+  par(mfrow = c(1, 1))
+}
+
+plot.dists <- function(a, b, cols = c('pop_A', 'pop_B'), nbins = 1000){
+  dat = c(a,b)
+  maxs = max(dat, na.rm = TRUE)
+  mins = min(dat, na.rm = TRUE)
+  breaks = seq(maxs, mins, length.out = (nbins + 1))
+  par(mfrow = c(2, 1))
+  hist(a, breaks = breaks, main = paste('Histogram of', cols[1]), xlab = cols[1])
+  abline(v = mean(a), lwd = 4, col = 'red')
+  hist(b, breaks = breaks, main = paste('Histogram of', cols[2]), xlab = cols[2])
+  abline(v = mean(b), lwd = 4, col = 'red')
+  par(mfrow = c(1, 1))
+}
+
+plot.dists.cut <- function(a, b, cols = c('pop_A', 'pop_B'), nbins = 1000){
+  maxs = max(c(max(a), max(b)))
+  mins = min(c(min(a), min(b)))
+  maxU <- max(quantile(a, probs = (1 - .05/2)), quantile(b, probs = (1 - .05/2)))
+  breaks = seq(maxs, mins, length.out = (nbins + 1))
+  par(mfrow = c(2, 1))
+  hist(a, breaks = breaks, main = paste('Histogram of', cols[1]), xlab = cols[1], xlim = c(0, maxU))
+  abline(v = mean(a), lwd = 4, col = 'red')
+  hist(b, breaks = breaks, main = paste('Histogram of', cols[2]), xlab = cols[2], xlim = c(0, maxU))
+  abline(v = mean(b), lwd = 4, col = 'red')
+  par(mfrow = c(1, 1))
+}
